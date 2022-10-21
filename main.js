ce6ee37e060ccda81c2dc491da0ea70b68fb76b6
Manuel Valls Fernández <me@manvalls.com>
2022-10-21 18:21:07 +0000

    Get robot position

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 import * as readline from 'node:readline'
 import * as process from 'node:process'
 
+const ROBOT_DIRECTIONS = ['N', 'E', 'S', 'W']
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -8,8 +10,22 @@ const rl = readline.createInterface({
 
 const getLine = () => new Promise((resolve) => rl.once('line', resolve))
 
+const getRobotPosition = async () => {
+  const line = await getLine()
+  const [, robotX, robotY, robotDirection] = line.match(/^(.+?)\s*(.+?)\s*(.+?)/)
+  return {
+    robotX: Number(robotX),
+    robotY: Number(robotY),
+    robotDirection: ROBOT_DIRECTIONS.indexOf(robotDirection),
+  }
+}
+
 ;(async () => {
   let line = await getLine()
   const [, topX, topY] = line.match(/^(.+?)\s*(.+?)/).map(Number)
-  console.log(topX, topY)
+
+  while(true) {
+    let { robotX, robotY, robotDirection } = await getRobotPosition()
+    console.log({ robotX, robotY, robotDirection })
+  }
 })()
