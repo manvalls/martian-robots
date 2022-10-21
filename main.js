7b81d6eba725374d9eacb1bef079f050d430b665
Manuel Valls Fernández <me@manvalls.com>
2022-10-21 18:34:52 +0000

    Add basic command logic

--- main.js
+++ main.js
@@ -3,6 +3,11 @@ import * as process from 'node:process'
 
 const ROBOT_DIRECTIONS = ['N', 'E', 'S', 'W']
 
+const NORTH = ROBOT_DIRECTIONS.indexOf('N')
+const EAST = ROBOT_DIRECTIONS.indexOf('E')
+const SOUTH = ROBOT_DIRECTIONS.indexOf('S')
+const WEST = ROBOT_DIRECTIONS.indexOf('W')
+
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
@@ -20,6 +25,43 @@ const getRobotPosition = async () => {
   }
 }
 
+const commands = {
+  DEFAULT: ({ robotX, robotY, robotDirection }) => ({ robotX, robotY, robotDirection }),
+
+  L: ({ robotX, robotY, robotDirection }) => ({
+    robotX,
+    robotY,
+    robotDirection: (robotDirection + 3) % 4,
+  }),
+
+  R: ({ robotX, robotY, robotDirection }) => ({
+    robotX,
+    robotY,
+    robotDirection: (robotDirection + 1) % 4,
+  }),
+
+  F: ({ robotX, robotY, robotDirection }) => {
+
+    if (robotDirection === NORTH) {
+      return { robotX, robotY: robotY + 1, robotDirection }
+    }
+
+    if (robotDirection === EAST) {
+      return { robotX: robotX + 1, robotY, robotDirection }
+    }
+
+    if (robotDirection === SOUTH) {
+      return { robotX, robotY: robotY - 1, robotDirection }
+    }
+
+    if (robotDirection === WEST) {
+      return { robotX: robotX - 1, robotY, robotDirection }
+    }
+
+    return { robotX, robotY, robotDirection }
+  },
+}
+
 ;(async () => {
   let line = await getLine()
   const [, topX, topY] = (line.match(/.*?(\d+)\s*(\d+).*?/) || []).map(Number)
@@ -28,7 +70,12 @@ const getRobotPosition = async () => {
     let { robotX, robotY, robotDirection } = await getRobotPosition()
     
     for (const command of await getLine()) {
-      console.log(command)
+      const commandFn = commands[command] || commands.DEFAULT
+      
+      ;({ robotX, robotY, robotDirection } = commandFn({ robotX, robotY, robotDirection }))
+
+      console.log(commandFn)
+      console.log(robotX, robotY, ROBOT_DIRECTIONS[robotDirection])
     }
   }
 })()
